22609e9785727c64f08c3097679aded89799567d
BhushJ <jagtapbhushan11@gmail.com>
2023-10-01 15:09:55 +0400

    validation for password on frontend

--- reactjs-nestjs-mongo/frontend/src/components/register.tsx
+++ reactjs-nestjs-mongo/frontend/src/components/register.tsx
@@ -7,15 +7,42 @@ export const Register = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [redirect, setRedirect] = useState(false);
+    const [validationError, setValidationError] = useState("");
+    const [isSubmitDisabled, setIsSubmitDisabled] = useState(true);
 
     const submit = async (e: SyntheticEvent) => {
       e.preventDefault();
-      await axios.post('register', {
-        name,
-        email,
-        password
-      });
-      setRedirect(true);
+      if(!isSubmitDisabled) {
+        await axios.post('register', {
+          name,
+          email,
+          password
+        });
+        setRedirect(true);  
+      }
+    };
+
+    const validatePassword = (value: string) => {
+      const minLength = 8;
+      const letterPattern = /[a-zA-Z]/;
+      const numberPattern = /[0-9]/;
+      const specialCharPattern = /[!@#$%^&*()_+{}\[\]:;<>,.?~\\-]/;
+  
+      if (
+        value.length < minLength ||
+        !letterPattern.test(value) ||
+        !numberPattern.test(value) ||
+        !specialCharPattern.test(value)
+      ) {
+        setValidationError(
+          "Password must contain at least 8 characters, 1 letter, 1 number, and 1 special character."
+        );
+        setIsSubmitDisabled(true);
+      } else {
+        setValidationError("");
+        setIsSubmitDisabled(false);
+      }
+
     };
 
     if(redirect) {
@@ -38,9 +65,14 @@ export const Register = () => {
         </div>
         <div className="form-floating">
           <input required type="password" className="form-control" id="floatingPassword" placeholder="Password" 
-            onChange={ e => setPassword(e.target.value)} />
+            onChange={ e => {
+                setPassword(e.target.value);
+                validatePassword(e.target.value);
+              }
+             } />
           <label htmlFor="floatingPassword">Password</label>
         </div>
+        {validationError && <p className="error">{validationError}</p>}
 
         <button className="btn btn-primary w-100 py-2" type="submit">Register</button>
         <p className="mt-5 mb-3 text-body-secondary">&copy; Bhushan Jagtap - 2023</p>
